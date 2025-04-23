78520b09b9c9e6e0ba697f7461951e40f45a3a08
my-name <iam.jake.a.castillo@gmail.com>
2025-04-23 14:59:43 +0800

    first commit

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Auth\Notifications\VerifyEmail;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
@@ -11,3 +12,4 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 
 Route::post('/register', [RegisteredUserController::class, 'store']);
 Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+Route::post('/verify',[VerifyEmail::class,'store']);
